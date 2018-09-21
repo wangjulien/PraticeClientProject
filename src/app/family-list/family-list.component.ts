1e1deffa2aca1e518d78fbf8528124fe00027c28
WANG <wangjiliang@gmail.com>
2018-09-22 01:26:27 +0200

    add components

---
+++ src/app/family-list/family-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-family-list',
+  templateUrl: './family-list.component.html',
+  styleUrls: ['./family-list.component.css']
+})
+export class FamilyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
